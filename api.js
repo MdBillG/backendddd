225d21c884c11bd9a2fda025f6e021ab87ca6abd
MdBillG <mdb8854@gmail.com>
2024-11-24 20:41:50 +0530

    d

--- api.js
+++ api.js
@@ -1,58 +1,22 @@
-const radius = [1, 2, 3, 4]
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-const area = function (radius) {
-    let output = []
-    for (let i = 0; i < radius.length; i++) {
-        output.push(radius[i] * radius[i])
-    }
-    return output
+let a = []
+
+for (let i = 0; i < numbers.length; i++) {
+    // a.push(numbers[i] * 2)
+    // console.log("objectssss", numbers[i])
 }
 
-// console.log(area(radius))
+// console.log("object", a)
 
-const sum = function (radius) {
-    let output = []
-    for (let i = 0; i < radius.length; i++) {
-        output.push(radius[i] + radius[i])
-    }
-    return output
+
+for (let number of numbers) {
+    // console.log("objezzzzzzzzzct", a.push(number * 0))
 }
 
-// console.log(sum(radius))
+let object = { a: 1, b: 1, c: 1, d: 1202020202, e: 1 }
 
-const product = function (radius) {
-    let output = []
-    for (let i = 0; i < radius.length; i++) {
-        output.push(radius[i] - radius[i])
-    }
-    return output
+for (let key in object) {
+    console.log("object", key, object[key])
 }
 
-// console.log(product(radius))
-
-
-const sumLogic = function (radius) {
-    return radius + radius
-
-}
-
-
-function commanFunction(radius, abc, sumLogic) {
-    let output = []
-    for (let i = 0; i < radius.length; i++) {
-        if (abc === 'sum') {
-            output.push(sumLogic(radius[i]))
-        }
-        else if (abc === 'product') {
-            output.push(radius[i] * radius[i])
-        }
-    }
-    return output
-}
-console.log(commanFunction(radius, 'sum', sumLogic))
-
-
-console.log("sdas", radius.map(sumLogic))
-
-
-const arr = [1, 2, 3, 4, 5, 6]
